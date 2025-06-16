ae16a474654b8aa02c4587e0d6fc8ac4e3f63ea3
Violetch <trueexpression9212@gmail.com>
2025-06-17 00:29:53 +0530

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+const App = () => {
+  return (
+    <div className="flex justify-center items-center h-[100svh] ">
+      <h1 className="text-2xl text-violet-500">Hello I'm Violet</h1>
+    </div>
+  );
+};
+
+export default App;
---
+++ src/routes/route.ts
@@ -0,0 +1,9 @@
+import {
+  createRootRoute,
+  createRoute,
+  createRouter,
+} from "@tanstack/react-router";
+
+const rootRoute = createRootRoute({
+    component: 
+})
