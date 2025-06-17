4dda337dbfb2272409e98ae35a614acb4848dd42
Violetch <trueexpression9212@gmail.com>
2025-06-18 01:21:41 +0700

    feat: Start making navbar

--- src/layouts/Navbar.tsx
+++ src/layouts/Navbar.tsx
@@ -1,8 +1,32 @@
+import { BsChevronDown } from "react-icons/bs";
+import { CiUser } from "react-icons/ci";
+import { IoIosSearch } from "react-icons/io";
+import { PiShoppingBagOpenThin } from "react-icons/pi";
+
 const Navbar = () => {
   return (
-    <div className="flex justify-center items-center absolute w-full py-7 text-neutral-100 bg-neutral-900">
-      <h1 className="font-semibold text-3xl">Navbar</h1>
-    </div>
+    <section className="flex justify-between items-center absolute w-full px-[200px] py-7 text-neutral-100 bg-neutral-900">
+      <div>
+        <ul className="flex justify-center items-center gap-7">
+          <li>All Products</li>
+          <li>Style</li>
+          <li className="flex items-center gap-2">
+            ScrollWorks
+            <span>
+              <BsChevronDown size={"11px"} />
+            </span>
+          </li>
+          <li>Discrod</li>
+          <li>Contact</li>
+          <li>About Us</li>
+        </ul>
+      </div>
+      <div className="flex justify-center items-center">
+        <IoIosSearch />
+        <CiUser />
+        <PiShoppingBagOpenThin />
+      </div>
+    </section>
   );
 };
 
